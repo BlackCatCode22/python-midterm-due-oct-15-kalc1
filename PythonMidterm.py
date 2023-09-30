e78381db3c0d59c83b564074bd924c3062a0bcf0
kalc1 <kalcocer1@gmail.com>
2023-09-29 21:29:41 -0700

    Create initial PythonMidterm.py file

---
+++ PythonMidterm.py
@@ -0,0 +1,17 @@
+# PythonMidterm.py
+#
+# Name: Kevin Alcocer
+# Date: 09/29/23
+# Class: CIT 95
+
+# def genBirthDay(): 
+#   """Calculate a birthday from the information received from the originating zoo. Handle cases where the birth season is unknown."""
+
+# def genUniqueAnimalID(): 
+#   """Calculate a unique ID to uniquely identify each animal in your zoo."""
+
+# def genAnimalName(): 
+#   """Create an animal name based on input from a community fundraiser (animalNames.txt)."""
+
+# def genZooHabitat(): 
+#   """Assign each new animal to a habitat. Each species must have its own habitat."""
