7dc66587897442315a31505e7897cb6b960d4d85
kalc1 <kalcocer1@gmail.com>
2023-10-04 15:24:47 -0700

    open arrivingAnimals.txt and input data into a list

--- PythonMidterm.py
+++ PythonMidterm.py
@@ -4,41 +4,52 @@
 # Date: 09/29/23
 # Class: CIT 95
 
-class Animal:
-    def __init__(self, name, age, sex, season_born, color, weight, location, country)
-    self.name = 
-    self.age = 
-    self.sex = 
-    self.season_born = 
-    self.color = 
-    self.weight = 
-    self.location = 
-    self.country = 
+#******* Start of Classes: 
+# class Animal:
+#     def __init__(self, name, age, sex, season_born, color, weight, location, country)
+#     self.name = 
+#     self.age = 
+#     self.sex = 
+#     self.season_born = 
+#     self.color = 
+#     self.weight = 
+#     self.location = 
+#     self.country = 
     
-class Hyena(Animal):
-    def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
-    super().__init__(name, age, sex, season_born, color, weight, location, country)
-    self.ID = 
-    self.habitat = (f"Hy"
+# class Hyena(Animal):
+#     def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
+#     super().__init__(name, age, sex, season_born, color, weight, location, country)
+#     self.ID = 
+#     self.habitat = (f"Hy"
 
-class Tiger(Animal):
-    def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
-    super().__init__(name, age, sex, season_born, color, weight, location, country)
-    self.ID = 
-    self.habitat = (f"Ti"
+# class Tiger(Animal):
+#     def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
+#     super().__init__(name, age, sex, season_born, color, weight, location, country)
+#     self.ID = 
+#     self.habitat = (f"Ti"
 
-class Lion(Animal):
-    def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
-    super().__init__(name, age, sex, season_born, color, weight, location, country)
-    self.ID = 
-    self.habitat = (f"Li" 
+# class Lion(Animal):
+#     def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
+#     super().__init__(name, age, sex, season_born, color, weight, location, country)
+#     self.ID = 
+#     self.habitat = (f"Li" 
 
-class Bear(Animal):
-    def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
-    super().__init__(name, age, sex, season_born, color, weight, location, country)
-    self.ID = 
-    self.habitat = (f"Be"
-    
+# class Bear(Animal):
+#     def __init__(self, name, age, sex, season_born, color, weight, location, country, habitat, ID)
+#     super().__init__(name, age, sex, season_born, color, weight, location, country)
+#     self.ID = 
+#     self.habitat = (f"Be"
+#*******End of Classes
+
+
+arrivingAnimalstxt = open('c:\\Users\\kalco\\Coding Projects\\Python\\python-midterm-due-oct-15-kalc1\\arrivingAnimals.txt', 'r')
+read_arriving = arrivingAnimalstxt.readlines()
+
+arrivingAnimals_list = []               # Turns the content in arrivingAnimals.txt into a list
+for animal in read_arriving: 
+    arrivingAnimals_list.append(animal.strip())
+print(arrivingAnimals_list)
+
 # def genBirthDay(): 
 #   """Calculate a birthday from the information received from the originating zoo. Handle cases where the birth season is unknown."""
 
